e8156eaf276ec72bc8c49a441bccc22224cb7b0c
atefganm <atef_anim_1974@yahoo.com>
2024-09-24 17:04:32 +0300

    add Display screenshot Thanks to @madie66

--- lib/python/Components/Lcd.py
+++ lib/python/Components/Lcd.py
@@ -43,6 +43,9 @@ class LCD:
 
 	def setFlipped(self, value):
 		eDBoxLCD.getInstance().setFlipped(value)
+		
+	def setScreenShot(self, value):
+ 		eDBoxLCD.getInstance().setDump(value)
 
 	def isOled(self):
 		return eDBoxLCD.getInstance().isOled()
